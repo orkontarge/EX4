32be6eef65eab594cc6ca0d87fa12bc444b87eb2
avigayilBrown <avigayil.wimpfheimer@mail.huji.ac.il>
2022-05-24 18:17:07 +0300

    sdf

---
+++ cmake-build-debug/CMakeFiles/EX4.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/avigayil/Documents/OS/EX4/PhysicalMemory.cpp" "CMakeFiles/EX4.dir/PhysicalMemory.cpp.o" "gcc" "CMakeFiles/EX4.dir/PhysicalMemory.cpp.o.d"
+  "/home/avigayil/Documents/OS/EX4/SimpleTest.cpp" "CMakeFiles/EX4.dir/SimpleTest.cpp.o" "gcc" "CMakeFiles/EX4.dir/SimpleTest.cpp.o.d"
+  "/home/avigayil/Documents/OS/EX4/VirtualMemory.cpp" "CMakeFiles/EX4.dir/VirtualMemory.cpp.o" "gcc" "CMakeFiles/EX4.dir/VirtualMemory.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/EX4.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/EX4.dir/PhysicalMemory.cpp.o"
+  "CMakeFiles/EX4.dir/PhysicalMemory.cpp.o.d"
+  "CMakeFiles/EX4.dir/SimpleTest.cpp.o"
+  "CMakeFiles/EX4.dir/SimpleTest.cpp.o.d"
+  "CMakeFiles/EX4.dir/VirtualMemory.cpp.o"
+  "CMakeFiles/EX4.dir/VirtualMemory.cpp.o.d"
+  "EX4"
+  "EX4.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/EX4.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
